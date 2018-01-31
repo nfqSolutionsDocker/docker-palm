7a95179f3f3ae0b4108e3887ee02f41beea683e4
manuel <manuel.iglesias@nfq.es>
2018-01-31 17:13:23 +0100

    first

---
+++ README.md
@@ -0,0 +1,37 @@
+### python CONTENEDOR PARA PALM
+
+This container has the following characteristics:
+
+- Python 3.4 installed.
+- Pip 3.4 installed.
+- Volumen is /solutions/app.
+
+Create into volumen '/solutions/app' the 'rpm' folder to put the rpm files to install in start container.
+Important, add in rpm files name the numbers order to install.
+
+Create into volumen '/solutions/app' the 'pip' folder to put the zip files to install in start container.
+Important, add in zip files name the numbers order to install.
+
+Add in docker-compose file 'PIP_PACKAGES' variable to install with pip 3.4 in start container.
+Add in docker-compose file 'PYTHON_FILES' variable to execute with python 3.4 in start container.
+
+For example, docker-compose.yml:
+```
+app:
+ image: nfqsolutions/python:3.4-jdk7
+ restart: always
+ environment:
+  - PACKAGES=
+  - PIP_PACKAGES=
+  - PYTHON_FILES=
+  - SERVICES=
+ ports:
+  - "8080:8888"
+ volumes:
+  - <mydirectory>:/solutions/app
+  - <mydirectory>/pip:/solutions/app/pip
+  - <mydirectory>/apps/rpm:/solutions/app/rpm
+ 
+```
+
+
---
+++ initial.py
@@ -0,0 +1,56 @@
+#!/usr/local/bin/python3
+
+import os
+import time
+
+os.system('sh /solutions/install_packages.sh')
+
+RPM_PATH = os.getenv('RPM_PATH')
+PIP_PACKAGES = os.getenv('PIP_PACKAGES')
+PIP_PATH = os.getenv('PIP_PATH')
+PYTHON_FILES = os.getenv('PYTHON_FILES')
+SERVICES = os.getenv('SERVICES')
+
+# FALSE if RPM_PATH is '' or None
+if bool(RPM_PATH):
+    rpm_file_list = os.listdir(RPM_PATH)
+    rpm_file_list.sort()
+    for file in rpm_file_list:
+        if file.endswith('.rpm'):
+            print('\033[1;32mInstalando ficheros RPM: {}\033[0m'.format(file))
+            os.system('rpm -Uvh {}/{}'.format(RPM_PATH,file))
+
+# FALSE if PIP_PACKAGES is '' or None
+if bool(PIP_PACKAGES):
+    for package in PIP_PACKAGES.split(','):
+        print('\033[1;32mInstalando paquetes oficiales en PIP: {}\033[0m'.format(package))
+        os.system('pip install {}'.format(package))
+
+# FALSE if PIP_PATH is '' or None
+if bool(PIP_PATH):
+    pip_file_list = os.listdir(PIP_PATH)
+    pip_file_list.sort()
+    for file in pip_file_list:
+        if file.endswith('.zip'):
+            print('\033[1;32mInstalando paquetes locales en PIP .zip: {}\033[0m'.format(file))
+            os.system('pip install {}/{}'.format(PIP_PATH, file))
+
+        if file.endswith('.tar.gz'):
+            print('\033[1;32mInstalando paquetes locales en PIP .tar.gz: {}\033[0m'.format(file))
+            os.system('pip install {}/{}'.format(PIP_PATH, file))
+
+# FALSE if PYTHON_FILES is '' or None
+if bool(PYTHON_FILES):
+    for file in PYTHON_FILES.split(','):
+        print('\033[1;32mEjecutando ficheros python: {}\033[0m'.format(file))
+        os.system('python {} &'.format(file))
+
+# FALSE if SERVICES is '' or None
+if bool(SERVICES):
+    for file in SERVICES.split(','):
+        print('\033[1;32mEjecutando service: {}\033[0m'.format(file))
+        os.system('{} &'.format(file))
+        time.sleep(5)
+
+print('\033[1;32mEjecutando bash ...\033[0m')
+os.system('/bin/bash')
